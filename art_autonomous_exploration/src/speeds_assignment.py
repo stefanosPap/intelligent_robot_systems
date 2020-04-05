fe32bcfc0c497ccf630dd191801d53e18cca4411
Stefanos <stefanospapadam@gmail.com>
2020-04-05 18:37:37 +0300

    Implements first obstacle avoidance version

--- art_autonomous_exploration/src/speeds_assignment.py
+++ art_autonomous_exploration/src/speeds_assignment.py
@@ -72,7 +72,95 @@ class RobotController:
       ############################### NOTE QUESTION ############################
       # Check what laser_scan contains and create linear and angular speeds
       # for obstacle avoidance
+      min_range = min(scan)
+      min_range_angle = scan.index(min(scan))
+    
+      max_range = max(scan)
+      max_range_angle = scan.index(max(scan))
+       
+      first_low_boundary = 0
+      first_up_boundary = len(scan)
+      second_low_boundary = -2.094 #minimum angle of a laser scan message 
+      second_up_boundary = 2.094 #maximum angle of a laser scan message
 
+      #map function to convert the indexes of scan array to the range of the real angles of the laser range 
+      #convert (0  -  scan's length) -> (-2.094  -  2.094)
+      map_fun = lambda x: (x - first_low_boundary)*(second_up_boundary - second_low_boundary) / (first_up_boundary - first_low_boundary) + second_low_boundary 
+      
+      min_range_angle = map_fun(min_range_angle)
+      max_range_angle = map_fun(max_range_angle)  
+     
+      #negative angular turns the robot right and positive left
+      
+      #speed's values are assigned in 0 - 100 range and at the end of the calculations they will be converted in the range -0.3  -  0.3
+      #using the map_fun_percent function
+      #-100 -> -0.3 , 0 -> 0 , 100 -> 0.3
+      angular = 0
+      linear = 60
+      #speed's weights are changing according to some criteria 
+      
+      #1st criterion 
+      count_right = 0
+      count_left = 0
+      for i in range(len(scan)):
+          if (scan[i] == max_range or scan[i] >= 0.7 * max_range):
+            if i < len(scan)/2:
+              count_right += 1 
+            else:
+              count_left += 1
+              
+      if count_right > count_left:
+        angular += -28
+      else:
+        angular += 28
+      #2nd criterion
+      if min_range < 1:
+        if min_range_angle < 0:
+          angular += 22
+       
+        else:
+          angular += -22
+          
+       #3rd criterion 
+      if min_range < 0.2:
+        if min_range_angle < 0:
+          angular += 30
+          linear = 10
+        else:
+          angular += -30
+          linear = 10
+          
+      #4th criterion 
+      count_crash_right=0
+      count_crash_left=0
+
+      for i in range(200,300):
+        if scan[i] < 0.4:
+          count_crash_right +=1
+
+      for i in range(300,400):
+        if scan[i] < 0.4:
+          count_crash_left +=1
+      
+      if (count_crash_left + count_crash_right) > 60:
+        if count_crash_left > count_crash_right:
+          angular += -20
+          linear = 0
+        else:
+          angular += 20
+          linear = 0
+      
+      first_low_boundary = -100 
+      first_up_boundary = 100
+      second_low_boundary = -0.3 #minimum angle of a laser scan message 
+      second_up_boundary = 0.3 #maximum angle of a laser scan message
+      map_fun_percent = lambda x: (x - first_low_boundary)*(second_up_boundary - second_low_boundary) / (first_up_boundary - first_low_boundary) + second_low_boundary       
+      
+      angular = map_fun_percent(angular)
+      linear = map_fun_percent(linear)
+      ##########################################################################
+      
+      return [linear, angular]
       ##########################################################################
       return [linear, angular]
 
@@ -114,7 +202,8 @@ class RobotController:
         ############################### NOTE QUESTION ############################
         # Implement obstacle avoidance here using the laser speeds.
         # Hint: Subtract them from something constant
-        pass
+        self.linear_velocity  = l_laser
+        self.angular_velocity = a_laser        
         ##########################################################################
 
     # Assistive functions
